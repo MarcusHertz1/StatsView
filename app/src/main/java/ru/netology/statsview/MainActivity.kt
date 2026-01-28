accf40b69d9b5b9c79486555f1192f0d00159791
Mark Reprintsev <audetore2013@gmail.com>
2026-01-28 21:54:12 +0300

    m7l6t1 add: basic animation

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -1,9 +1,10 @@
 package ru.netology.statsview
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.databinding.MainLayoutBinding
-import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: MainLayoutBinding
@@ -11,6 +12,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = MainLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        findViewById<StatsView>(R.id.statsView).data = listOf(500F, 500F, 500F, 500F)
+        binding.statsView.data = listOf(500F, 500F, 500F, 500F)
+        binding.statsView.startAnimation(AnimationUtils.loadAnimation(this, R.anim.animation)
+            .apply {
+                setAnimationListener(object :Animation.AnimationListener{
+                    override fun onAnimationEnd(animation: Animation?) {
+                        binding.label.text = "onAnimationEnd"
+                    }
+
+                    override fun onAnimationRepeat(animation: Animation?) {
+                        binding.label.text = "onAnimationRepeat"
+                    }
+
+                    override fun onAnimationStart(animation: Animation?) {
+                        binding.label.text = "onAnimationStart"
+                    }
+
+                })
+            })
     }
 }
