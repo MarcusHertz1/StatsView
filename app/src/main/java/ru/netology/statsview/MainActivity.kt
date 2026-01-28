7631715b11e2bfd9d28d4f6f46f48454a0acd22a
Mark Reprintsev <audetore2013@gmail.com>
2026-01-28 19:22:17 +0300

    m7l5t1 refactor: customize StatsView for incoming data

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.netology.statsview
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.statsview.databinding.MainLayoutBinding
+import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: MainLayoutBinding
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = MainLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        findViewById<StatsView>(R.id.statsView).data = listOf(0.25F, 0.25F, 0.25F, 0.25F)
     }
 }
